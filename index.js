9f2e84aa41692db9f75896d579d69c2736039cd6
Nguyen Huy <scythedead.h@gmail.com>
2020-03-12 23:23:19 +0700

    import library passport passport-google-oauth20 & generate google api oauth2.0

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+// clientID 899338678945-mrqu994ja1p3jktbfov2f383hrprmj78.apps.googleusercontent.com
+// clientSecret I3TWwXzJNSLLCY9-ng9PQgSm
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
