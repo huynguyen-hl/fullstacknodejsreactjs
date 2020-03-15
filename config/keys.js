b863d4d0aac4140b39415e8e0d95472902d4b6ab
Nguyen Huy <scythedead.h@gmail.com>
2020-03-15 23:14:29 +0700

    add authenticate passportjs with google oauth2.0 and routes handle login

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleClientID: '899338678945-mrqu994ja1p3jktbfov2f383hrprmj78.apps.googleusercontent.com',
+  googleClientSecret: 'I3TWwXzJNSLLCY9-ng9PQgSm'
+}
--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('./services/passport');
 
 const app = express();
 
-// clientID 899338678945-mrqu994ja1p3jktbfov2f383hrprmj78.apps.googleusercontent.com
-// clientSecret I3TWwXzJNSLLCY9-ng9PQgSm
-passport.use(new GoogleStrategy());
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
